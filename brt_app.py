a3cbc39b8e2dca0a60a35d870325102a41f9ac31
Victor Noronha <vgarritano@cos.ufrj.br>
2024-07-18 11:10:42 -0300

    update logic

--- brt_app.py
+++ brt_app.py
@@ -1,82 +1,107 @@
-import streamlit as st
+"""Streamlit App for next arrival predictions at BRT Vaz Lobo station"""
 
+from math import floor
+
+import pandas as pd
 import requests
-import time
+from requests import Response
 
 import folium
 from folium.map import Icon
+
+import streamlit as st
 from streamlit_folium import st_folium
 
-import pandas as pd
 
 st.title("O BRT tá chegando em Vaz Lobo?")
 
 
 @st.cache_data(
     ttl=60,
-    show_spinner="Puxando os dados da API do BRT...",
+    show_spinner="Consultando os dados da API do BRT...",
 )
-def get_data():
-    # tcvlo = requests.get("https://api.mobilidade.rio/gtfs/stops/?stop_code=TCVLO")
-    # response_json = tcvlo.json()
-    # loc = [
-    #     response_json["results"][0]["stop_lat"],
-    #     response_json["results"][0]["stop_lon"],
-    # ]
+def get_data() -> list[list[float, float], Response]:
+    """Get predictions of next arrivals at Vaz Lobo station from the BRT API
 
-    loc = [-22.85647, -43.32815]
+    Returns:
+       location (list[float, float]): The location of Vaz Lobo station
+       response (Response): The predictions for next BRT arrivals
+    """
 
-    results = requests.get("https://api.mobilidade.rio/predictor/?stop_id=3084BC0001U2")
+    vaz_lobo_station_location = [-22.85647, -43.32815]
 
-    return loc, results
+    response = requests.get(
+        "https://api.mobilidade.rio/predictor/?stop_id=3084BC0001U2", timeout=30
+    )
+
+    return vaz_lobo_station_location, response
 
 
 loc, results = get_data()
 
-next_brts = (
-    pd.DataFrame(results.json()["results"])[
-        ["codigo", "dataHora", "trip_short_name", "estimated_time_arrival"]
-    ]
-    .sort_values("estimated_time_arrival")
-    .query("estimated_time_arrival > 0")
-    .rename(
-        columns={
-            "trip_short_name": "linha",
-            "estimated_time_arrival": "previsão de chegada (minutos)",
+if results.status_code != 200:
+    st.error("Não foi possível recuperar os dados da API... Tente novamente mais tarde")
+
+    json_results = results.json()
+
+    st.json(
+        {
+            "status_code": results.status_code,
+            "code": json_results["code"],
+            "message": json_results["message"],
         }
-    )[["codigo", "linha", "previsão de chegada (minutos)"]]
-)
+    )
 
-next_arrival = next_brts.head(1)["previsão de chegada (minutos)"].item()
-minutes = int(next_arrival)
-seconds = (next_arrival - minutes) * 60
+else:
+    json_results = results.json()["results"]
 
-st.info(
-    f"O próximo BRT deve estar chegando em {minutes} minuto(s) e {seconds:.0f} segundo(s)..."
-)
+    brt_predictions = (
+        pd.DataFrame(json_results)[
+            ["codigo", "dataHora", "trip_short_name", "estimated_time_arrival"]
+        ]
+        .sort_values("estimated_time_arrival")
+        .query("estimated_time_arrival > 0")
+        .rename(
+            columns={
+                "trip_short_name": "linha",
+                "estimated_time_arrival": "previsão de chegada (minutos)",
+            }
+        )
+        .reset_index(drop=True)[["codigo", "linha", "previsão de chegada (minutos)"]]
+    )
 
-st.dataframe(next_brts)
+    next_arrival = brt_predictions["previsão de chegada (minutos)"].iloc[0]
+    minutes = int(next_arrival)
+    seconds = (next_arrival - minutes) * 60
 
-vaz_lobo = folium.Map(
-    location=loc,
-    zoom_start=12,
-    scrollWheelZoom=False,
-)
+    st.info(
+        f"O próximo BRT deve estar chegando em {minutes} minuto(s) e {seconds:.0f} segundo(s)..."
+    )
 
-for result in results.json()["results"]:
-    if result["estimated_time_arrival"] <= 0:
-        continue
+    st.dataframe(brt_predictions)
+
+    vaz_lobo = folium.Map(
+        location=loc,
+        zoom_start=12,
+        scrollWheelZoom=False,
+    )
+
+    for result in json_results:
+        if result["estimated_time_arrival"] <= 0:
+            continue
+
+        arrival_time = floor(result["estimated_time_arrival"])
+
+        folium.Marker(
+            location=[result["latitude"], result["longitude"]],
+            icon=Icon(color="orange", prefix="fa", icon="bus"),
+            tooltip=f"Tempo de chegada desse BRT: {arrival_time:.0f} min",
+        ).add_to(vaz_lobo)
 
     folium.Marker(
-        location=[result["latitude"], result["longitude"]],
-        icon=Icon(color="orange", prefix="fa", icon="bus"),
-        tooltip=f"ETA: {result['estimated_time_arrival']:.2f} min",
+        location=loc,
+        tooltip="Estação BRT Vaz Lobo",
+        icon=Icon(color="blue", prefix="fa", icon="signs-post"),
     ).add_to(vaz_lobo)
 
-folium.Marker(
-    location=loc,
-    tooltip="Estação BRT Vaz Lobo",
-    icon=Icon(color="blue", prefix="fa", icon="signs-post"),
-).add_to(vaz_lobo)
-
-st_data = st_folium(vaz_lobo)
+    st_data = st_folium(vaz_lobo)
